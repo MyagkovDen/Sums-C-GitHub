b2215a622394ba00909d96ef874cae3ed069e1b8
Denis Myagkov <tomich-84@mail.ru>
2022-11-12 01:48:18 +0300

    add Sum004

--- LinearAlgorithms/Sum001/Program.cs
+++ LinearAlgorithms/Sum001/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("13   24   83   9");
+﻿int a = new Random().Next(1,123);
+int b = new Random().Next(4,667);
+int c = new Random().Next(12,90);
+int d = new Random().Next(56,99);
+Console.WriteLine($"{a}   {b}   {c}   {d}");
