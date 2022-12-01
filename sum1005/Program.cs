934079a0f470d9f1c6a40d93435c418a530a07cb
Denis Myagkov <tomich-84@mail.ru>
2022-12-02 00:41:19 +0300

    sum1005

---
+++ sum1005/Program.cs
@@ -0,0 +1,24 @@
+﻿// Известны два расстояния: одно в километрах, другое – в футах (1 фут 0,305 м). Какое из расстояний меньше?
+
+double km = GetNumberFromUser("Введите число: ", "Ошибка ввода!");
+double foot = GetNumberFromUser("Введите число: ", "Ошибка ввода!");
+
+if(km*3278.69 > foot)
+Console.WriteLine($"{foot} футов меньше");
+else
+Console.WriteLine($"{km} км меньше");
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+
+        else
+            Console.WriteLine(errorMessage);
+    }
+}
